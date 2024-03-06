a3b4d7d1bdd5e63939d27992f7ce653809f00ae7
Vigintillionn <yarne.ramakers@gmail.com>
2024-03-06 13:11:53 +0100

    Refactor main.rs module structure and optimize algorithm testing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 mod algorithms;
-mod types;
 mod generate_array;
 mod plotter;
+mod types;
 
-use std::vec;
+use rayon::prelude::*;
 use std::thread;
 use std::time::Instant;
-use rayon::prelude::*;
+use std::vec;
 
-use algorithms::selection::SelectionSort;
 use algorithms::insertion::InsertionSort;
 use algorithms::merge::MergeSort;
-use types::{Algorithm, AlgorithmData};
+use algorithms::selection::SelectionSort;
 use plotter::plot_comparisons;
+use types::{Algorithm, AlgorithmData};
 
 const STEP_SIZE: usize = 200;
 
@@ -21,6 +21,7 @@ fn main() {
     let insertion = InsertionSort::new();
     let merge = MergeSort::new();
 
+    // Spawn threads for each algorithm
     let selection_thread = thread::spawn(|| {
         let _ = test_algorithm(selection);
     });
@@ -34,14 +35,18 @@ fn main() {
     });
 
     let start = Instant::now();
+
+    // Wait for all threads to finish
     selection_thread.join().unwrap();
     insertion_thread.join().unwrap();
     merge_thread.join().unwrap();
+
     let duration = start.elapsed();
     println!("Time elapsed is: {:?}", duration);
 }
 
 fn test_algorithm(algorithm: impl Algorithm + Sync) -> Result<(), Box<dyn std::error::Error>> {
+    // Test each case in parallel
     algorithm.get_cases().into_par_iter().for_each(|case| {
         println!("Testing {} case: {}", algorithm.get_name(), case.clone().name);
         test_case(&algorithm, case.clone());
@@ -51,21 +56,20 @@ fn test_algorithm(algorithm: impl Algorithm + Sync) -> Result<(), Box<dyn std::e
     Ok(())
 }
 
-fn test_case(
-    algorithm: &(impl Algorithm + Sync),
-    case: AlgorithmData
-) {
+fn test_case(algorithm: &(impl Algorithm + Sync), case: AlgorithmData) {
     let mut data = vec![];
+    // Test each array length
     for i in (0..=10000).step_by(STEP_SIZE) {
         let mut arr = (case.generator)(i);
         let comparisons = algorithm.sort(&mut arr);
         data.push((i, comparisons));
     }
+    // Plot the results
     plot_comparisons(
-        data, 
-        algorithm.get_name(), 
-        case.clone().name, 
+        data,
+        algorithm.get_name(),
+        case.clone().name,
         format!("{}-{}", algorithm.get_name(), case.name),
-        case.expected
+        case.expected,
     );
 }
