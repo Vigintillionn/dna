3001b5e74d39aaa97f87c502b921d999e6e3ec44
Vigintillionn <yarne.ramakers@gmail.com>
2024-03-10 12:35:28 +0100

    Added margin to plotter

--- src/plotter.rs
+++ src/plotter.rs
@@ -3,20 +3,21 @@ use plotters::prelude::*;
 use crate::types::{Expected, ExpectedData};
 
 pub fn plot_comparisons(
-  data: Vec<(usize, usize)>, 
+  data: Vec<(usize, Vec<usize>)>, 
   name: String, 
   case: String, 
   filename: String,
   expected: ExpectedData,
 ) {
   let name_with_extension = format!("plots/{}.png", filename);
-  let root = BitMapBackend::new(&name_with_extension, (800, 600)).into_drawing_area();
+  let root = BitMapBackend::new(&name_with_extension, (1200, 1000)).into_drawing_area();
   root.fill(&WHITE).unwrap();
 
-  let (x, y): (Vec<usize>, Vec<usize>) = data.into_iter().unzip();
+  let (x, y): (Vec<usize>, Vec<Vec<usize>>) = data.clone().into_iter().unzip();
 
   let max_x = *x.iter().max().unwrap();
-  let max_y = *y.iter().max().unwrap();
+  // let max_y = *y.iter().max().unwrap();
+  let max_y = *y.iter().map(|v| v.iter().max().unwrap()).max().unwrap();
 
   let title = format!("{} - {}", name, case);
 
@@ -33,22 +34,41 @@ pub fn plot_comparisons(
     .x_labels(10)
     .y_labels(10)
     .x_desc("Array Length")
+    .y_desc("Comparisons")
     .axis_desc_style(("sans-serif", 15))
     .draw()
     .unwrap();
 
+  // chart
+  //   .draw_series(PointSeries::of_element(x.into_iter().zip(y.into_iter()), 3, &BLUE, &|c, s, st| {
+  //       let avg_y = (c.1.iter().sum::<usize>() as f64 / c.1.len() as f64) as usize;
+  //       return EmptyElement::at((c.0, avg_y)) + Circle::new((0,0), s, st.filled());
+  //   }))
+  //   .unwrap();
+
+  // chart.draw_series(LineSeries::new(
+  //     data.iter().map(|(x, y)| (*x, y.iter().sum::<usize>() / y.len())),
+  //     &BLUE,
+  // ));
+
+
   chart
-    .draw_series(PointSeries::of_element(x.into_iter().zip(y.into_iter()), 3, &BLUE, &|c, s, st| {
-        return EmptyElement::at(c) + Circle::new((0,0), s, st.filled());
-    }))
-    .unwrap();
+    .draw_series(
+      data.into_iter().map(|(x, y)| {
+        let yl = y.iter().min().unwrap();
+        let ym = &y[y.len() / 2];
+        let yh = y.iter().max().unwrap();
+
+        return ErrorBar::new_vertical(x, *yl, *ym, *yh, BLUE.filled(), 4)
+      }),
+    ).unwrap().label("Measured").legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLUE));
 
   chart
     .draw_series(LineSeries::new(
       (0..=max_x).map(|x| (x, apply_expected(expected.clone().function, expected.clone().factor, x as f64))),
       &RED
     ))
-    .unwrap();
+    .unwrap().label("Expected").legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLUE));
 }
 
 fn apply_expected(expected: Expected, factor: f64, x: f64) -> usize {
